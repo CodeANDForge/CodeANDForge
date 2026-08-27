a2cbbf222c9eccc4037707de89f17f2562885fd3
Ahmed <a7medzcvvv@gmail.com>
2026-08-27 17:14:32 +0300

    Add ADMINS setting for mail_admins to work

--- config/settings.py
+++ config/settings.py
@@ -200,5 +200,6 @@ EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 ADMIN_NOTIFICATION_EMAIL = config("ADMIN_NOTIFICATION_EMAIL", default="")
+ADMINS = [("Admin", ADMIN_NOTIFICATION_EMAIL)] if ADMIN_NOTIFICATION_EMAIL else []
 
 FACEBOOK_PAGE_URL = "https://www.facebook.com/share/18ie8XWUJa/"
